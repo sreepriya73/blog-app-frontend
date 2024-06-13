db8bc1796ef2e7a220e6af8a444242f62e22bdc7
sreepriya73 <sreepriyaillikkatt@gmail.com>
2024-06-13 13:45:55 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
+import Signin from './components/Signin';
 
 function App() {
   return (
     <div>
       <Signup/>
+      <Signin/>
     </div>
   );
 }
